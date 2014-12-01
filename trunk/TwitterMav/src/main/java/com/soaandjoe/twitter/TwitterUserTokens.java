c4499b106bb5ee0d27c9eaf730dc1010c7d05cbe
oathmo@gmail.com <oathmo@gmail.com@2a77cf63-a867-722c-9506-59117099e28f>
2014-12-01 12:52:59 +0000

    Commit Inicial

---
+++ trunk/TwitterMav/src/main/java/com/soaandjoe/twitter/TwitterUserTokens.java
@@ -0,0 +1,33 @@
+package com.soaandjoe.twitter;
+
+/**
+ *
+ * @author Joel
+ */
+public class TwitterUserTokens {
+
+    private String token;
+    private String tokenSecret;
+
+    public TwitterUserTokens(String token, String tokenSecret) {
+        this.token = token;
+        this.tokenSecret = tokenSecret;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenSecret() {
+        return tokenSecret;
+    }
+
+    public void setTokenSecret(String tokenSecret) {
+        this.tokenSecret = tokenSecret;
+    }
+
+}
---
+++ trunk/TwitterMav/src/main/java/com/soaandjoe/twitter/UrlKey.java
@@ -0,0 +1,43 @@
+package com.soaandjoe.twitter;
+
+/**
+ *
+ * @author Joel
+ */
+public class UrlKey {
+
+    private String url;
+    private String token;
+    private String tokenSecret;
+
+    public UrlKey(String url, String token, String tokenSecret) {
+        this.url = url;
+        this.token = token;
+        this.tokenSecret = tokenSecret;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenSecret() {
+        return tokenSecret;
+    }
+
+    public void setTokenSecret(String tokenSecret) {
+        this.tokenSecret = tokenSecret;
+    }
+
+}
